93a41e9381185a501c93c35a2226f8727a4d51ef
Prabesh <tamangprabesh360@gmail.com>
2022-09-08 07:02:22 +0545

    Calculater

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+Console.WriteLine("Enter first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
